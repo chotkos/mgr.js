8c26e4dc346bdf0b901b750607f077f824e7193b
chotkos <chotkos@gmail.com>
2016-07-11 09:19:45 +0200

    mgr.interpolate

---
+++ mgr.interpolate.js
@@ -0,0 +1,54 @@
+var interpolate = {
+	mappedElements: [],
+	mapObjects: [],
+	render : function(element){
+		var all = document.getElementsByTagName("*");
+		for(var i = 0; i < all.length; i++){
+			if(all[i].attributes["mgr"]){
+				this.mappedElements.push(all[i]);
+
+				var interpKeys = eval(all[i].getAttribute("mgr"));
+				//<div mgr="['css color scope.red', 'text scope.name']"></div>
+				//element should also has scope property
+				for(var l=0;l<interpKeys.length;l++){
+					var interpKey = interpKeys[l];
+					var splitArray = interpKey.split(' ');
+					var method = splitArray[0];
+					splitArray.shift();
+					var params = splitArray;
+					var o = {
+						element:all[i],
+						interpKey:interpKey,
+						method : method,
+						params : params,
+						oldValue : null,					
+					};						
+					
+					o.oldValue = this.getMapObjectValue(all[i]);
+					
+					this.mapObjects.push(o);
+				}
+			
+				console.log(all[i]);
+			}
+		}
+	},
+	timerFunction: function(){
+		//foreach all mapObjects
+		
+		//if oldvalue another than current -> run modifications on objects.
+	},
+	timerRun:function(){
+		setInterval(this.timerFunction(), 500);
+	},
+	getMapObjectValue(mo) { 
+		var o = null;
+		if (params > 0) {
+			o = mo[method](params[0]);
+		} else { 
+			o = mo[method]();
+		}
+		return o;
+	}
+
+};
