a65f599130ea35cc33292be329d79a0a349f65cd
Mateusz Chodkowski <Mateusz Chodkowski>
2016-10-23 14:17:24 +0200

    example changes

--- examples/crudApp/controllers.js
+++ examples/crudApp/controllers.js
@@ -2,15 +2,12 @@ var userlist = new View("userlist", function (scope) {    console.log("userlist"
     scope.users = dataRepository.getAll();
     scope.showProfile = function(id){
         viewManager.renderView('userprofile',false,{id:id});
-        //alert(id);
     };
 
 
 }, "userlist.html", true);
 
 var userprofile = new View("userprofile", function (scope, routeParams) {
-    console.log("userprofile");
-    console.log(routeParams);
     scope.user = dataRepository.getById(parseInt(routeParams.id));
     scope.moveBack = function(){
         if(viewManager.lastViewName != null){
