db5c9080fcb6f8169a848a4b3443d345f63528bc
Mateusz Chodkowski <Mateusz Chodkowski>
2016-10-23 13:51:28 +0200

    userprofile start

--- examples/crudApp/controllers.js
+++ examples/crudApp/controllers.js
@@ -1,8 +1,8 @@
 var userlist = new View("userlist", function (scope) {    console.log("userlist");
     scope.users = dataRepository.getAll();
     scope.showProfile = function(id){
-        //viewManager.renderView('userprofile',false,{id:id});
-        alert(id);
+        viewManager.renderView('userprofile',false,{id:id});
+        //alert(id);
     };
 
 
@@ -10,5 +10,8 @@ var userlist = new View("userlist", function (scope) {    console.log("userlist"
 
 var userprofile = new View("userprofile", function (scope, routeParams) {
     console.log("userprofile");
+    console.log(routeParams);
+    scope.user = dataRepository.getById(parseInt(routeParams.id));
+
 }, "userprofile.html", false);
 mgrStart();
