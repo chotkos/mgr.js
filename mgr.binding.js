048fae3714380f006e34e1ab0b30f67c80731baa
Mateusz Chodkowski <chotkos@gmail.com>
2017-02-01 17:23:59 +0100

    refactor part 2

--- mgr.binding.js
+++ mgr.binding.js
@@ -10,120 +10,127 @@ var binding = {
 
         //<a mgr-events="['click scope.moveBack()']">Back</a>
         if (renderedElement.attributes["mgr-events"]) {
-            var interpValues = eval(renderedElement.getAttribute("mgr-events"));
-            for (var z = 0; z < interpValues.length; z++) {
-                var split = interpValues[z].split(' ');
-                var ind = interpValues[z].indexOf(' ');
-                var fun = interpValues[z].substring(ind);
-                fun = fun.replaceAll('scope', 'element.scope');
-                //fun.replaceAll('')
-                $(renderedElement).on(split[0], function () {
-                    eval(fun);
-                });
-            }
+            this.renderEvents(renderedElement,element);
         }
         //<div mgr-repeat="item in scope.items">
         if (renderedElement.attributes["mgr-repeat"]) {
-
-            var interpValues = renderedElement.getAttribute("mgr-repeat").split(" ");
-            var alias = interpValues[0];
-            var collectionName = interpValues[2];
-            var collectionData = JSON.parse(JSON.stringify(eval(collectionName.replaceAll('scope', 'element.scope'))));
-            var template = renderedElement;
-            var tplHtml = $(renderedElement).html();
-            var parent = renderedElement.parentNode;
-
-            //mark template elements to not map
-            var toMark = template.getElementsByTagName("*");
-            for (var zi = 0; zi < toMark.length; zi++) {
-                $(toMark[zi]).attr('isTemplate', 'true');
-            };
-
-            template.hidden = true;
-
-            for (var k = 0; k < collectionData.length; k++) {
-                var e = document.createElement(template.tagName);
-                $(e).html(tplHtml);
-                e.scope = collectionData[k];
-                $(e).html($(e).html().replaceAll(alias, collectionName + '[' + k + ']'));
-                $(parent).append(e);
-                var all = [];
-                all.push.apply(all, e.getElementsByTagName("*"));
-                for (var ww = 0; ww < all.length; ww++) {
-                    this.renderElement(all[ww], element, viewName);
-                }
-            }
-
-            this.repeatObjects.push({
-                viewName: viewName,
-                element: renderedElement,
-                data: collectionData,
-                renderElement: element,
-                collectionName: collectionName.replaceAll('scope', 'element.scope'),
-            });
-
+            this.renderRepeat(renderedElement, element, viewName);
         } else
         //<div mgr="['css color scope.color', 'text scope.name']"></div>
         if (renderedElement.attributes["mgr"] && !renderedElement.hasAttribute("istemplate")) {
-            if (jQuery.contains(document, renderedElement)) {
-                this.mappedElements.push(renderedElement);
-                var interpKeys = eval(renderedElement.getAttribute("mgr"));
-                for (var l = 0; l < interpKeys.length; l++) {
-                    var interpKey = interpKeys[l];
-                    var splitArray = interpKey.split(' ');
-                    var method = splitArray[0];
-                    splitArray.shift();
-                    var params = splitArray;
-                    var o = {
-                        viewName: viewName,
-                        element: renderedElement,
-                        interpKey: interpKey,
-                        method: method,
-                        params: params,
-                        oldValueOnElement: null,
-                        oldValueOnResource: null,
-                        scopeOwner: element,
-                        mapFromResource: function () {
-                            var p1 = this.params[0] ? this.params[0].replaceAll('scope.', 'this.scopeOwner.scope.') : '';
-                            var p2 = this.params[1] ? this.params[1].replaceAll('scope.', 'this.scopeOwner.scope.') : '';
-                            if (p1.indexOf('scope') != -1) {
-                                p1 = eval(p1);
-                            }
-                            if (p2.indexOf('scope') != -1) {
-                                p2 = eval(p2);
-                            }
-                            eval("$(this.element)[this.method](p1,p2)");
-                        },
-                        mapFromElement: function () {
-                            var field = null;
-                            var value = null;
-                            if (this.params.length > 1) {
-                                field = "this.scopeOwner." + this.params[1];
-                                value = "$(this.element)[this.method](this.params[0])";
-                            } else {
-                                field = "this.scopeOwner." + this.params[0];
-                                value = "$(this.element)[this.method]()";
-                            }
-                            eval(field + "=" + value);
-                        },
-                    };
-                    o.mapFromResource();
-                    o.oldValueOnElement = this.getMapObjectoldValueOnElement(o);
-                    o.oldValueOnResource = this.getMapObjectoldValueOnResource(o);
-                    if (this === null) {
-                        this.mapObjects.push(o);
-                    } else {
-                        this.mapObjects.push(o);
-                    }
-                }
-            }
+            this.renderProperties(renderedElement, viewName, element);
         } else
         //<div mgr-dir="mydir scopefieldname">
         if (renderedElement.attributes["mgr-dir"]) {
             this.renderDirective(renderedElement, this, element, viewName);
         }
     },
+    renderRepeat: function (renderedElement, element, viewName) {
+        var interpValues = renderedElement.getAttribute("mgr-repeat").split(" ");
+        var alias = interpValues[0];
+        var collectionName = interpValues[2];
+        var collectionData = JSON.parse(JSON.stringify(eval(collectionName.replaceAll('scope', 'element.scope'))));
+        var template = renderedElement;
+        var tplHtml = $(renderedElement).html();
+        var parent = renderedElement.parentNode;
 
+        //mark template elements to not map
+        var toMark = template.getElementsByTagName("*");
+        for (var zi = 0; zi < toMark.length; zi++) {
+            $(toMark[zi]).attr('isTemplate', 'true');
+        }
+        ;
+
+        template.hidden = true;
+
+        for (var k = 0; k < collectionData.length; k++) {
+            var e = document.createElement(template.tagName);
+            $(e).html(tplHtml);
+            e.scope = collectionData[k];
+            $(e).html($(e).html().replaceAll(alias, collectionName + '[' + k + ']'));
+            $(parent).append(e);
+            var all = [];
+            all.push.apply(all, e.getElementsByTagName("*"));
+            for (var ww = 0; ww < all.length; ww++) {
+                this.renderElement(all[ww], element, viewName);
+            }
+        }
+
+        this.repeatObjects.push({
+            viewName: viewName,
+            element: renderedElement,
+            data: collectionData,
+            renderElement: element,
+            collectionName: collectionName.replaceAll('scope', 'element.scope'),
+        });
+    },
+    renderProperties: function (renderedElement, viewName, element) {
+        if (jQuery.contains(document, renderedElement)) {
+            this.mappedElements.push(renderedElement);
+            var interpKeys = eval(renderedElement.getAttribute("mgr"));
+            for (var l = 0; l < interpKeys.length; l++) {
+                var interpKey = interpKeys[l];
+                var splitArray = interpKey.split(' ');
+                var method = splitArray[0];
+                splitArray.shift();
+                var params = splitArray;
+                var o = {
+                    viewName: viewName,
+                    element: renderedElement,
+                    interpKey: interpKey,
+                    method: method,
+                    params: params,
+                    oldValueOnElement: null,
+                    oldValueOnResource: null,
+                    scopeOwner: element,
+                    mapFromResource: function () {
+                        var p1 = this.params[0] ? this.params[0].replaceAll('scope.', 'this.scopeOwner.scope.') : '';
+                        var p2 = this.params[1] ? this.params[1].replaceAll('scope.', 'this.scopeOwner.scope.') : '';
+                        if (p1.indexOf('scope') != -1) {
+                            p1 = eval(p1);
+                        }
+                        if (p2.indexOf('scope') != -1) {
+                            p2 = eval(p2);
+                        }
+                        eval("$(this.element)[this.method](p1,p2)");
+                    },
+                    mapFromElement: function () {
+                        var field = null;
+                        var value = null;
+                        if (this.params.length > 1) {
+                            field = "this.scopeOwner." + this.params[1];
+                            value = "$(this.element)[this.method](this.params[0])";
+                        } else {
+                            field = "this.scopeOwner." + this.params[0];
+                            value = "$(this.element)[this.method]()";
+                        }
+                        eval(field + "=" + value);
+                    },
+                };
+                o.mapFromResource();
+                o.oldValueOnElement = this.getMapObjectoldValueOnElement(o);
+                o.oldValueOnResource = this.getMapObjectoldValueOnResource(o);
+                if (this === null) {
+                    this.mapObjects.push(o);
+                } else {
+                    this.mapObjects.push(o);
+                }
+            }
+        }
+    },
+    renderEvents: function (renderedElement,element) {
+        var interpValues = eval(renderedElement.getAttribute("mgr-events"));
+        for (var z = 0; z < interpValues.length; z++) {
+            var split = interpValues[z].split(' ');
+            var ind = interpValues[z].indexOf(' ');
+            var fun = interpValues[z].substring(ind);
+            fun = fun.replaceAll('scope', 'element.scope');
+            //fun.replaceAll('')
+            $(renderedElement).on(split[0], function () {
+                eval(fun);
+            });
+        }
+    },
     renderDirective: function (directiveElement, context, mainElement, viewName) {
         //split data from attribute
         var atr = directiveElement.getAttribute("mgr-dir");
